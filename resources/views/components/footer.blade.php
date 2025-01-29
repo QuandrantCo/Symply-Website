801e51813ff349a624d691b9ea69c0e70cb63b5f
Abian Fikri <boyanonim22@gmail.com>
2025-01-29 19:28:01 +0800

    style(footer):  fix footer using CSS

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,43 @@
-<div>
-    <div>Ini Footer Update</div>
+<div class="container-footer">
+    {{-- Footer Logo --}}
+    <div class="footer-logo">
+        <img src="{{ asset('storage/image/logo-symply.svg') }}" alt="Symply Haircut">
+
+        <div class="dots">
+            <div class="dots-item">
+
+            </div>
+            <div class="dots-item">
+
+            </div>
+            <div class="dots-item">
+
+            </div>
+        </div>
+    </div>
+
+    {{-- Contact Us Footer --}}
+    <div class="footer-contact">
+        <h3>Contact Us</h3>
+        <p>Phone Number: 08xxxxxxxxx</p>
+        <p>Email: abcd@gmail.com</p>
+        <p>
+            Yogyakarta (Kasihan Area)
+            <br />
+            Rukeman Jl. Sunan Kudus No.5a,
+            <br />
+            Kasihan, Bantul, Yogyakarta 55184
+        </p>
+        <div class="footer-map">
+            <i class="fas fa-map-marker-alt"></i>
+            <p>View Map</p>
+        </div>
+    </div>
+
+    <div class="footer-opening">
+        <h3>Opening Hours</h3>
+        <p>
+            Mon-Sun, 09.30 AM - 10.00 PM
+        </p>
+    </div>
 </div>
