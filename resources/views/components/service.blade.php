aed5c72a44755727d9d3505563e6f9777b66908e
aldy202 <aldyfauzi202@.gmail.com>
2025-03-18 19:44:27 +0700

    feat(service-icon): add hover in service icon and resonsive modal

--- resources/views/components/service.blade.php
+++ resources/views/components/service.blade.php
@@ -12,32 +12,55 @@
         </div>
         <div class="content-service">
             <div class="card-service">
-                <img src="{{ asset('images/service/service-icon.png') }}" alt="Service 1" id="myBtn">
-                <img src="{{ asset('images/service/service-icon.png') }}" alt="Service 2">
-                <img src="{{ asset('images/service/service-icon.png') }}" alt="Service 3">
-                <img src="{{ asset('images/service/service-icon.png') }}" alt="Service 4">
-                <img src="{{ asset('images/service/service-icon.png') }}" alt="Service 5">
-                <img src="{{ asset('images/service/service-icon.png') }}" alt="Service 6">
-                <img src="{{ asset('images/service/service-icon.png') }}" alt="Service 7">
-                <img src="{{ asset('images/service/service-icon.png') }}" alt="Service 8">
+                <!-- Add hover effect with different modal id for each image -->
+                <img src="{{ asset('images/service/service-icon.png') }}" alt="Service 1" class="service-image"
+                    data-modal="myModal1">
+                <img src="{{ asset('images/service/service-icon.png') }}" alt="Service 2" class="service-image"
+                    data-modal="myModal2">
+                <img src="{{ asset('images/service/service-icon.png') }}" alt="Service 3" class="service-image"
+                    data-modal="myModal3">
+                <img src="{{ asset('images/service/service-icon.png') }}" alt="Service 4" class="service-image"
+                    data-modal="myModal4">
+                <img src="{{ asset('images/service/service-icon.png') }}" alt="Service 5" class="service-image"
+                    data-modal="myModal5">
+                <img src="{{ asset('images/service/service.png') }}" alt="Service 6" class="service-image"
+                    data-modal="myModal6">
+                <img src="{{ asset('images/service/Creambath-icon.svg') }}" alt="Service 7" class="service-image"
+                    data-modal="myModal7">
+                <img src="{{ asset('images/service/Creambath-icon.png') }}" alt="Service 8" class="service-image"
+                    data-modal="myModal8">
             </div>
         </div>
 
-        <div id="myModal" class="modal">
-            <!-- Modal Content -->
+        <!-- Modal Content for Service 1 -->
+        <div id="myModal1" class="modal">
             <div class="modal-content">
-                <!-- Close Button -->
                 <span class="close">&times;</span>
                 <div class="modal-body">
-                    <!-- Main logo and description (Left Side) -->
                     <div class="main-logo">
-                        <img src="{{ asset('images/service/service-icon.png') }}" alt="Service 8">
+                        <img src="{{ asset('images/service/service-icon.png') }}" alt="Service 1">
                         <h2 class="model-title">CREAMBATH</h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-                            been the industry's standard dummy text ever since the 1500s...</p>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
                     </div>
+                    <div class="small-logos">
+                        <img src="{{ asset('images/service/Logo-symply.png') }}" alt="Logo 1" class="logo-small">
+                        <img src="{{ asset('images/service/Logo-symply.png') }}" alt="Logo 2" class="logo-small">
+                        <img src="{{ asset('images/service/Logo-symply.png') }}" alt="Logo 3" class="logo-small">
+                    </div>
+                </div>
+            </div>
+        </div>
 
-                    <!-- Small logos positioned vertically (Right Side) -->
+        <!-- Repeat similar modal for other services -->
+        <div id="myModal2" class="modal">
+            <div class="modal-content">
+                <span class="close">&times;</span>
+                <div class="modal-body">
+                    <div class="main-logo">
+                        <img src="{{ asset('images/service/service-icon.png') }}" alt="Service 2">
+                        <h2 class="model-title">SERVICE 2</h2>
+                        <p>Details about service 2...</p>
+                    </div>
                     <div class="small-logos">
                         <img src="{{ asset('images/service/Logo-symply.png') }}" alt="Logo 1" class="logo-small">
                         <img src="{{ asset('images/service/Logo-symply.png') }}" alt="Logo 2" class="logo-small">
@@ -50,29 +73,32 @@
 </div>
 @push('js')
     <script>
-        var modal = document.getElementById("myModal");
+        // Get all images in the card-service
+        var images = document.querySelectorAll('.service-image');
 
-        // Get the button that opens the modal
-        var btn = document.getElementById("myBtn");
+        images.forEach(function(image) {
+            image.addEventListener('mouseenter', function() {
+                var modalId = image.getAttribute('data-modal');
+                var modal = document.getElementById(modalId);
+                modal.style.display = "block"; // Show the modal
+            });
+        });
 
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
+        // Get all modal close buttons and close the modal when clicked
+        var closeBtns = document.querySelectorAll('.close');
+        closeBtns.forEach(function(btn) {
+            btn.addEventListener('click', function() {
+                var modal = btn.closest('.modal');
+                modal.style.display = "none"; // Close the modal
+            });
+        });
 
-        // When the user clicks on the button, open the modal
-        btn.onclick = function() {
-            modal.style.display = "block";
-        }
-
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
-            modal.style.display = "none";
-        }
-
-        // When the user clicks anywhere outside of the modal, close it
+        // Close the modal if clicked outside of the modal
         window.onclick = function(event) {
-            if (event.target == modal) {
+            var modal = event.target.closest('.modal');
+            if (modal && event.target == modal) {
                 modal.style.display = "none";
             }
-        }
+        };
     </script>
 @endpush
