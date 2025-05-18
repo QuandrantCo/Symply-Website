a83354e93501943c1d74b1b465e63ff526047220
aldy202 <aldyfauzi202@.gmail.com>
2025-05-18 21:45:11 +0700

    tambahan informasi pada academy

--- resources/views/components/academy.blade.php
+++ resources/views/components/academy.blade.php
@@ -3,44 +3,196 @@
         <link rel="stylesheet" href="{{ asset('css/academy.css') }}">
         <style>
             .background-academy {
-                background-image: url('{{ asset('storage/images/academy/back-academy.svg') }}');
+                background-image: url('{{ asset('storage/images/academy/bg-academy.svg') }}');
             }
         </style>
     @endpush
     <div class="academy">
         <div class="background-academy">
             <div class="container-academy">
-                <div class="title-academy">
-                    <h2 id="text"></h2>
+                <div class="container-title-academy">
+                    <div class="title-academy">
+                        <h2 id="text">SIMPLY HAIRCUT ACADEMY</h2>
+                    </div>
                 </div>
-                <div class="sub-title-academy">
-                    <p>WE ARE PREPARING SOMETHING AMAZING ACADEMY FOR YOU!</p>
-                    <p><span>✨</span> Stay Tuned!<span>✨</span></p>
+            </div>
+        </div>
+        <div class="container-sub-academy">
+            <div class="title-sub-academy">
+                <h2>The Art of barbering</h2>
+                <h2>Jadilah Barber Profesional yang berkualitas</h2>
+            </div>
+        </div>
+        <div class="container-content-academy">
+            <div class="content-title-academy">
+                <p>Selamat datang di Simply Haircut Academy, tempat pelatihan barber terkemuka di Indonesia!
+                    Kami membantu Anda mengembangkan keterampilan dan kreativitas dalam bidang barbering,
+                    sehingga Anda dapat menjadi barber profesional yang memiliki kualitas dalam setiap cukuran.
+                </p>
+            </div>
+            <div class="container-faq">
+                <div class="faq">
+                    <button class="accordion">
+                        Mengapa Memilih Kami ?
+                        <i class="fa-solid fa-chevron-down"></i>
+                    </button>
+                    <div class="pannel">
+                        <p>• Kurikulum yang dirancang oleh ahli barber yang memiliki jam terbang tinggi
+                        </p>
+
+                        <p>• Kami memiliki Pengajar berpengalaman yang pernah bekerja di beberapa barber besar
+                            di
+                            indonesia
+                        </p>
+                        <p>• Anda akan diajarkan tentang pola suatu cukuran hingga memperoleh hasil yang baik
+                            dan
+                            konsisten
+                        </p>
+
+                        <p>• Fasilitas pelatihan yang modern dan lengkap
+                        </p>
+
+                        <p>• Kesempatan untuk berlatih dengan klien real dan membangun portofolio anda kedepan</p>
+                        
+
+                        <p>• Dukungan penuh untuk membantu Anda memulai karir sebagai barber
+                        </p>
+                    </div>
+                </div>
+                <div class="faq">
+                    <button class="accordion">
+                        Program Pelatihan Kami
+                        <i class="fa-solid fa-chevron-down"></i>
+                    </button>
+                    <div class="pannel">
+                        <p>• Basic Barbering Course: Dasar-dasar barbering dan teknik dasar mencukur
+                        </p>
+
+                        <p>• Advanced Barbering Course: Teknik lanjutan dan desain rambut modern
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+            <div class="container-end">
+                <div class="title-end">
+                    <p>Bergabunglah dengan Kami!</p>
+                    <p>Daftarkan diri Anda sekarang juga dan mulai jalani karir sebagai barber profesional
+                        yang berkualitas</p>
+                </div>
+            </div>
+
+        </div>
+        <div class="container-card">
+            <div class="card-title">
+                <h2>Academy Courses</h2>
+            </div>
+            <div class="card-content">
+                <div class="card">
+                    <div class="title">Beginner I</div>
+                    <div class="subtitle">(14 Pertemuan)</div>
+                    <div class="price-old">Rp 3.000.000</div>
+                    <div class="price-new">Rp 2.500.000</div>
+                    <div class="course-title">Course:</div>
+                    <ul class="course-list">
+                        <li>Haircut (Sidepart, Quiff, Korean Style)</li>
+                        <li>Taper (Low, Mid, High, Temple Taper)</li>
+                        <li>Treatment (Creambath)</li>
+                        <li>Styling</li>
+                    </ul>
+                </div>
+
+                <div class="card">
+                    <div class="title">Beginner II</div>
+                    <div class="subtitle">(20 Pertemuan)</div>
+                    <div class="price-old">Rp 3.500.000</div>
+                    <div class="price-new">Rp 3.000.000</div>
+                    <div class="course-title">Course:</div>
+                    <ul class="course-list">
+                        <li>Haircut (Sidepart, Quiff, Korean Style)</li>
+                        <li>Taper (Low, Mid, High, Temple Taper)</li>
+                        <li>Treatment (Creambath, Masker &amp; totok wajah)</li>
+                        <li>Styling</li>
+                    </ul>
+                </div>
+
+                <div class="card">
+                    <div class="title">Regular</div>
+                    <div class="subtitle">(30 Pertemuan)</div>
+                    <div class="price-old">Rp 5.500.000</div>
+                    <div class="price-new">Rp 5.000.000</div>
+                    <div class="course-title">Course:</div>
+                    <ul class="course-list">
+                        <li>Haircut (Sidepart, Quiff, Korean Style)</li>
+                        <li>Taper (Low, Mid, High, Temple Taper)</li>
+                        <li>Fading (Lowfade, Midfade, Highfade, Dropfade)</li>
+                        <li>Treatment (Creambath, Masker &amp; totok wajah)</li>
+                        <li>Styling</li>
+                        <li>Free Clipper / Alat Cukur</li>
+                    </ul>
+                </div>
+
+                <div class="card">
+                    <div class="title">Pro</div>
+                    <div class="subtitle">(30 Pertemuan)</div>
+                    <div class="price-old">Rp 6.500.000</div>
+                    <div class="price-new">Rp 6.000.000</div>
+                    <div class="course-title">Course:</div>
+                    <ul class="course-list">
+                        <li>Haircut (Sidepart, Quiff, Korean Style)</li>
+                        <li>Taper (Low, Mid, High, Temple Taper)</li>
+                        <li>Fading (Lowfade, Midfade, Highfade, Dropfade)</li>
+                        <li>Treatment (Creambath, Masker &amp; totok wajah)</li>
+                        <li>Styling</li>
+                        <li>Teknik Gunting (Longtrim)</li>
+                        <li>Coloring</li>
+                        <li>Free Clipper / Alat Cukur</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="container-daftar">
+            <div class="title-daftar">
+                <h2>Cara Pendaftaran</h2>
+            </div>
+            <div class="content-daftar">
+                <div class="list">
+                    <ul>
+                        <li>1. Hubungi admin di 085169765567</li>
+                        <li>2. Pilih paket yang diinginkan</li>
+                        <li>3. Dp 50% Bisa ke lokasi / via transfer</li>
+                        <li>4. Pelunasan Administrasi membawa foto 4x6 ( 2 lembar), Fotocopy Ktp (1 lembar)</li>
+                    </ul>
+                </div>
+                <div class="sub-list">
+                    <p>*Pelunasan dilakukan saat pertama pembelajaran</p>
+                </div>
+                <div class="button-daftar">
+                    <a href="https://wa.me/085169765567" target="_blank"><button>Pendaftaran <i class="fa-solid fa-arrow-right"></i></button></a>
                 </div>
             </div>
         </div>
     </div>
-    @push('js')
-        <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.7/dist/gsap.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.7/dist/TextPlugin.min.js"></script>
+</div>
+@push('js')
+    <script>
+        var acc = document.getElementsByClassName("accordion");
+        var i;
 
-        <script>
-            var text = document.getElementById("text");
-            document.addEventListener("DOMContentLoaded", (event) => {
-                gsap.registerPlugin(TextPlugin);
-                gsap.to(text, {
-                    duration: 3,
-                    text: "COMING SOON..",
-                    ease: "power2.out",
-                });
-                gsap.from(".sub-title-academy", {
-                    duration: 2,
-                    opacity: 0,
-                    y: 30,
-                    ease: "power2.out",
-                    delay: 1.5
-                });
+        for (i = 0; i < acc.length; i++) {
+            acc[i].addEventListener("click", function() {
+                this.classList.toggle("active");
+                this.parentElement.classList.toggle("active");
+
+                var pannel = this.nextElementSibling;
+
+                if (pannel.style.display === "block") {
+                    pannel.style.display = "none";
+                } else {
+                    pannel.style.display = "block";
+                }
             });
-        </script>
-    @endpush
+        }
+    </script>
+@endpush
 </div>
